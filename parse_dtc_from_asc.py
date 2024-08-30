70947fed69a7249674eca807df28aa27d47b2380
moschiel <roger.moschiel.ext@michelinconnectedfleet.com>
2024-08-30 10:03:07 -0300

    also updated progress bar on 'show complete timeline' mode

--- parse_dtc_from_asc.py
+++ parse_dtc_from_asc.py
@@ -7,6 +7,12 @@ from tkinter import ttk
 
 file_path = 'example_files/VWConstel2024_1.asc'
 # file_path = 'example_files/VWConstel2024_2.asc'
+# file_path = 'example_files/Accelo2023-2024_817.asc'
+# file_path = 'example_files/Atego_2024_666kbs.asc'
+# file_path = 'example_files/Atego03-07-24SaoGabriel.asc'
+# file_path = 'example_files/Daf_BoaViagem_BDE8B87.asc'
+# file_path = 'example_files/DAF_E6.asc'
+# file_path = 'example_files/DAFBoa viagem.asc'
 
 # Print control variables
 PRINT_DM1_SINGLE_FRAME = False
@@ -30,10 +36,12 @@ timeline_faults = []
 debounce_fault_inactive = 10 # Remove faults that have not been updated by this amount of time (seconds)
 debounce_fault_active_count = 10  # Number of occurrences to consider fault active
 debounce_fault_active_time = 10  # Time window in seconds to consider fault active
+timeout_multi_frame = 5
 
 # Control variables for emulating time
 last_time = 0.0 
 last_displayed_timestamp = 0.0
+start_time = 0
 end_time = 0
 
 # GUI reference
@@ -233,9 +241,6 @@ def update_active_faults(src, spn, fmi, cm, oc, mil, rsl, awl, pl, timestamp):
 # Remove faults that have not been updated for a certain time
 # or if they are not 'candidate' anymore
 def remove_inactive_faults(timestamp):
-    # if app_mode != 'EMULATE_TIME':
-        # return
-
     global active_faults
     global debounce_fault_inactive
     global changedFaultList
@@ -313,17 +318,17 @@ def clear_control_variables():
     root.update_idletasks()
 
 def update_screen_time(timestamp):
-    if app_mode != 'EMULATE_TIME':
-        return
+    #if app_mode != 'EMULATE_TIME':
+    #    return
     global last_displayed_timestamp
-    if timestamp - last_displayed_timestamp >= 1:
+    if timestamp - last_displayed_timestamp >= 1: # at least 1 second
         last_displayed_timestamp = timestamp
         global timestamp_label
-        global end_time
+        global start_time, end_time
         global progress_var
         global root
-        timestamp_label.config(text=f"Time: {timestamp}s")
-        percent = (timestamp / end_time) * 100
+        percent = ((timestamp - start_time) / (end_time - start_time)) * 100
+        timestamp_label.config(text=f"Time: {timestamp}s ({int(percent)}%)")
         progress_var.set(percent)
         root.update_idletasks()
         if(percent >= 100):
@@ -331,15 +336,22 @@ def update_screen_time(timestamp):
             stop_thread = True
 
 # Function to get the end timestamp from the log file
-def get_end_time(file_path):
+def get_start_and_end_time(file_path):
     with open(file_path, 'r') as file:
+        start_timestamp = 0
+        end_timestamp = 0
         lines = file.readlines()
+        for line in lines:
+            if 'Rx' in line:
+                parts = line.split()
+                start_timestamp = float(parts[0])
+                break
         for line in reversed(lines):
             if 'Rx' in line:
                 parts = line.split()
-                timestamp = float(parts[0])
-                return timestamp
-    return 0
+                end_timestamp = float(parts[0])
+                break
+    return int(start_timestamp), int(end_timestamp)
 
 # Main function to read log file and print DTCs from individual frames or from BAM frames
 def read_log_and_print_dtc(file_path):
@@ -355,7 +367,7 @@ def read_log_and_print_dtc(file_path):
                 break
 
             if started_measurement == False:
-                if 'Start of measurement' in line:
+                if 'Rx   d' in line:
                     started_measurement = True
                 else:
                     continue
@@ -378,8 +390,8 @@ def read_log_and_print_dtc(file_path):
                     if PRINT_J1939TP_FECAp:
                         print(line.strip())
 
-                message_id = parts[2]  # CAN ID
-                src = message_id.zfill(8)[6:8]  # source, last byte of CAN ID
+                message_id = parts[2].replace("x", "").zfill(8)  # CAN ID
+                src = message_id[6:8]  # source, last byte of CAN ID
 
                 if is_dm1_message_id(message_id):
                     data_bytes = [int(b, 16) for b in parts[6:14]]
@@ -406,7 +418,8 @@ def read_log_and_print_dtc(file_path):
                                 current_bams.remove(bam)
 
                         current_bams.append({
-                            'timestamp': timestamp,
+                            'first_seen': int_timestamp,
+                            'last_seen': int_timestamp,
                             'message_id': message_id,
                             'message_id_tp_dt': message_id_tp_dt,
                             'total_size': total_size,
@@ -415,7 +428,7 @@ def read_log_and_print_dtc(file_path):
                             'packets': []
                         })
                         if PRINT_TP_CM:
-                            print(f"[{timestamp}] TP.CM -> ID: {message_id}, Size: {total_size} bytes, Number of Packets: {num_packets}, PGN: {pgn:#X}")
+                            print(f"[{int_timestamp}] TP.CM -> ID: {message_id}, Size: {total_size} bytes, Number of Packets: {num_packets}, PGN: {pgn:#X}")
                 elif is_tp_dt_message_id(message_id):  # Identify TP.DT message
                     result = parse_tp_dt_message(line)
                     if result:
@@ -424,14 +437,15 @@ def read_log_and_print_dtc(file_path):
                         for bam in current_bams:
                             if bam['message_id_tp_dt'] == message_id:
                                 if PRINT_TP_DT:
-                                    print(f"[{timestamp}] TP.DT -> ID: {message_id}, Packet Number: {packet_number} of {bam['num_packets']}, Data: {' '.join(data)}")
+                                    print(f"[{int_timestamp}] TP.DT -> ID: {message_id}, Packet Number: {packet_number} of {bam['num_packets']}, Data: {' '.join(data)}")
                                 if packet_number != (len(bam['packets']) + 1):
                                     if PRINT_INCORRET_ORDER:
-                                        print(f'[{timestamp}] Packet Order is Incorrect, ID: {(message_id.replace('x','',1))}, Received: {packet_number}, Expected: {(len(bam['packets']) + 1)}')
+                                        print(f'[{int_timestamp}] Packet Order is Incorrect, ID: {(message_id.replace('x','',1))}, Received: {packet_number}, Expected: {(len(bam['packets']) + 1)}')
                                     current_bams.remove(bam)
                                     break
 
                                 bam['packets'].append((packet_number, data))
+                                bam['last_seen'] = int_timestamp
                                 if len(bam['packets']) == bam['num_packets']:
                                     bam['packets'].sort()
                                     combined_data = []
@@ -440,10 +454,10 @@ def read_log_and_print_dtc(file_path):
                                     combined_data = combined_data[:bam['total_size']]
 
                                     if PRINT_TP_DM1_MULTI_FRAME:
-                                        print(f"[{timestamp}] TP CONCAT -> ID: {(bam['message_id'].replace('x','',1))}, Size: {bam['total_size']}, Data: {' '.join(combined_data)}")
+                                        print(f"[{int_timestamp}] TP CONCAT -> ID: {(bam['message_id'].replace('x','',1))}, Size: {bam['total_size']}, Data: {' '.join(combined_data)}")
                                     
                                     data_bytes = [int(b, 16) for b in combined_data]
-                                    parse_dm1_message(timestamp, src, data_bytes)
+                                    parse_dm1_message(int_timestamp, src, data_bytes)
                                     
                                     current_bams.remove(bam)
                                     break
@@ -453,19 +467,27 @@ def read_log_and_print_dtc(file_path):
                     last_timestamp = int_timestamp
                     emulate_waiting_time(int_timestamp)
                     update_screen_time(int_timestamp)
-                    check_faults(int_timestamp)
+                    check_faults(int_timestamp, current_bams)
 
-def check_faults(timestamp):
+def remove_incomplete_multi_frame_message(timestamp, bams):
+    for bam in bams:
+        if (timestamp - bam['last_seen']) > timeout_multi_frame:
+            print(f"[{timestamp}] discard incomplete multiframe, CM: {bam['message_id']}, DT: {bam['message_id_tp_dt']}, FirstSeen: {bam['first_seen']}, LastSeen: {bam['last_seen']}")
+            bams.remove(bam)
+
+
+def check_faults(timestamp, bams):
     remove_inactive_faults(timestamp)
+    remove_incomplete_multi_frame_message(timestamp, bams)
     global changedFaultList
     if changedFaultList:
         changedFaultList = False
-        update_active_faults_display()
+        update_active_faults_display(timestamp)
         print_active_faults()
 
 # Dictionary to store references to treeview items
 treeview_items = {}
-def update_active_faults_display():
+def update_active_faults_display(timestamp):
     if app_mode != 'EMULATE_TIME':
         return
 
@@ -490,7 +512,7 @@ def update_active_faults_display():
         if key not in current_fault_keys:
             values = tree.item(treeview_items[key], 'values')
             # Atualizar a segunda coluna (índice 1 / status) para um novo valor 'inactive'
-            new_values = (values[0], 'inactive', *values[2:])
+            new_values = (0, timestamp, 'inactive', *values[3:])
             tree.item(treeview_items[key], values=new_values)
             tree.item(treeview_items[key], tags=('inactive',))
 
@@ -643,8 +665,8 @@ def init_app():
     progress_bar = ttk.Progressbar(progress_frame, variable=progress_var, maximum=100)
     progress_bar.pack(side=tk.LEFT, padx=5, fill=tk.X, expand=True)
 
-    global end_time
-    end_time = get_end_time(file_path)
+    global start_time, end_time
+    start_time, end_time = get_start_and_end_time(file_path)
 
     def on_closing():
         global stop_thread
